d9e6290fa8bfaf663e371dfef7626b9255b26dc6
Judit Escofet <90208829+ololololololoj@users.noreply.github.com>
2022-07-15 20:47:10 +0200

    Add files via upload

---
+++ website/scripts.js
@@ -0,0 +1,164 @@
+const jsongs = {
+    "songs":[
+        {"name": "Tired of Sex", 
+        "spoty": "https://open.spotify.com/track/42hDrA9GTkuvpxjLtAshct?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=THDc2MAMwok&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/tired-of-sex/1440869116?i=1440869121",
+        "deezr": "https://www.deezer.com/track/17613315?autoplay=true",
+        "genius": "https://genius.com/Weezer-tired-of-sex-lyrics"
+    },
+        {"name": "Getchoo", 
+        "spoty": "https://open.spotify.com/track/1rvbY7HXYsFXSlLqxphocK?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=_8ESeMaT1jc&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/getchoo/1440869116?i=1440869124",
+        "deezr": "https://www.deezer.com/track/17613316?autoplay=true",
+        "genius": "https://genius.com/Weezer-getchoo-lyrics"
+    },
+        {"name": "No Other One", 
+        "spoty": "https://open.spotify.com/track/3pdwtknFmM6xocVEpj7yAl?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=F8VwP5OvJ3o&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/no-other-one/1440898333?i=1440898583",
+        "deezr": "https://www.deezer.com/track/2960594?autoplay=true",
+        "genius": "https://genius.com/Weezer-no-other-one-lyrics"
+    },
+        {"name": "Why Bother?", 
+        "spoty": "https://open.spotify.com/track/43Qr1YBCY3vSVCvZuR0PCb?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=w9cOmtP5wxg&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/why-bother/1440898333?i=1440898588",
+        "deezr": "https://www.deezer.com/track/7310039/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-why-bother-lyrics"
+    },
+        {"name": "Across the Sea", 
+        "spoty": "https://open.spotify.com/track/7JJPh9eOiSr8zYlBjcu2w0?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=uDPmykX7HR8&feature=share",
+        "apple": "https://music.apple.com/es/album/across-the-sea-piano-noodles/1440869116?i=1440869607",
+        "deezr": "https://www.deezer.com/track/7310044/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-across-the-sea-lyrics"
+    },
+        {"name": "The Good Life", 
+        "spoty": "https://open.spotify.com/track/5CjwXdq5Z73Va0XCPFG5Aq?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=NbRinqYBLYs&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/the-good-life/1440898333?i=1440898755",
+        "deezr": "https://www.deezer.com/track/2960597/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-the-good-life-lyrics"
+    },
+        {"name": "El Scorcho", 
+        "spoty": "https://open.spotify.com/track/3g2gQMeeQAEPztiQKMlGSl?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=9Asco6xgp_k&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/el-scorcho/1624848657?i=1624849056",
+        "deezr": "https://www.deezer.com/track/2960598/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-el-scorcho-lyrics"
+    },
+        {"name": "Pink Triangle", 
+        "spoty": "https://open.spotify.com/track/6qQGxKyy6LcyZVsWn93lyS?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=v2vWDWxYJBc&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/pink-triangle-live-at-reading-festival-1996/1440869116?i=1440869598",
+        "deezr": "https://www.deezer.com/track/2960599/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-pink-triangle-lyrics"
+    },
+        {"name": "Falling For You", 
+        "spoty": "https://open.spotify.com/track/1MnXNW03vkijBz0GHAsHgW?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=hdrAsZ7ebuc&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/falling-for-you/1440898333?i=1440898873",
+        "deezr": "https://www.deezer.com/track/2960600/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-falling-for-you-lyrics"
+    },
+        {"name": "Butterfly", 
+        "spoty": "https://open.spotify.com/track/3PYNwzHABIkJxA6hPcf9bT?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=yHNVm9FzgCI&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/butterfly/1440869116?i=1440869300",
+        "deezr": "https://www.deezer.com/track/2960601/radio?autoplay=true",
+        "genius": "https://genius.com/Weezer-butterfly-lyrics"
+    },
+        {"name": "You Gave Your Love To Me Softly", 
+        "spoty": "https://open.spotify.com/track/6nfLKum9Lr2AuLkeDMoJ8e?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=ASDzIXCRVQI&feature=gws_kp_trackhttps://music.apple.com/es/album/you-gave-your-love-to-me-softly/1440869116?i=1440869304",
+        "apple": "https://music.apple.com/es/album/you-gave-your-love-to-me-softly/1440869116?i=1440869304",
+        "deezr": "https://www.deezer.com/track/17613292?autoplay=true",
+        "genius": "https://genius.com/Weezer-you-gave-your-love-to-me-softly-lyrics"
+    },
+        {"name": "Devotion", 
+        "spoty": "https://open.spotify.com/track/1tZeHEchoo62G0xpEcjbAj?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=_y8FjHOpPNw&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/devotion/1440869116?i=1440869307",
+        "deezr": "https://www.deezer.com/track/7310026?autoplay=true",
+        "genius": "https://genius.com/Weezer-devotion-lyrics"
+    },
+        {"name": "Waiting On You", 
+        "spoty": "https://open.spotify.com/track/3ovTVlWIAzB1XGG8dWPtFl?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=ilM1i7vvqFs&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/waiting-on-you/1440869116?i=1440869316",
+        "deezr": "https://www.deezer.com/track/582307312?autoplay=true",
+        "genius": "https://genius.com/Weezer-waiting-on-you-lyrics"
+    },
+        {"name": "I Just Threw Out The Love Of My Dreams", 
+        "spoty": "https://open.spotify.com/track/35SRuRfp5BvD1yArmXKNHO?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=r2dosVRzLSM&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/i-just-threw-out-the-love-of-my-dreams/1440869116?i=1440869319",
+        "deezr": "https://www.deezer.com/track/17613296?autoplay=true",
+        "genius": "https://genius.com/Weezer-i-just-threw-out-the-love-of-my-dreams-lyrics"
+    },
+        {"name": "I Swear It's True", 
+        "spoty": "https://open.spotify.com/track/0oaQ4ExTz7RUB4dkRg3eyC?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=Hvmi129D074&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/i-swear-its-true/1440869116?i=1440869446",
+        "deezr": "https://www.deezer.com/track/17613299?autoplay=true",
+        "genius": "https://genius.com/Weezer-i-swear-its-true-lyrics"
+    },
+        {"name": "You Won't Get With Me Tonight", 
+        "spoty": "https://open.spotify.com/track/2ywyHcaPWCOHopio9QsWmE?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=jRUjfDcL3iA&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/you-wont-get-with-me-tonight/1440869116?i=1440869459",
+        "deezr": "https://www.deezer.com/track/7310035?autoplay=true",
+        "genius": "https://genius.com/Weezer-you-wont-get-with-me-tonight-lyrics"
+    },
+        {"name": "Long Time Sunshine", 
+        "spoty": "https://open.spotify.com/track/2pY4j2aJ5nyw0IjhgpeAEW?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=44USiU5Nglo&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/long-time-sunshine/1440869116?i=1440869613",
+        "deezr": "https://www.deezer.com/track/17613313?autoplay=true",
+        "genius": "https://genius.com/Weezer-long-time-sunshine-lyrics"
+    },
+        {"name": "Getting Up And Leaving", 
+        "spoty": "https://open.spotify.com/track/2ax0HKoaMTrJBFX9gEC9mE?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=jguic7-Azvs&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/getting-up-and-leaving/1440869116?i=1440869617",
+        "deezr": "https://www.deezer.com/track/649753892?autoplay=true",
+        "genius": "https://genius.com/Weezer-getting-up-and-leaving-lyrics"
+    },
+        {"name": "Tragic Girl", 
+        "spoty": "https://open.spotify.com/track/2J4Q2WIx3572uZuTTNkCby?autoplay=true",
+        "ytmus": "https://music.youtube.com/watch?v=nr_3L6M-Hsk&feature=gws_kp_track",
+        "apple": "https://music.apple.com/es/album/tragic-girl/1440869116?i=1440869745",
+        "deezr": "https://www.deezer.com/track/17613317?autoplay=true",
+        "genius": "https://genius.com/Weezer-tragic-girl-lyrics"
+    },
+    ]
+}
+
+let isPress = false
+
+function randSong() {
+    const albumImg = document.getElementById("albumImg");
+    const links = document.getElementById("links");
+    const spoty = document.getElementById("spoty");
+    const yt = document.getElementById("yt");
+    const apple = document.getElementById("apple");
+    const deezer = document.getElementById("deezer");
+    const cover = document.getElementById("cover");
+    const songName = document.getElementById("songName");
+    
+    if (!isPress) {
+        albumImg.src = "https://www.covermesongs.com/wp-content/uploads/2016/09/Weezer_Pinkerton.jpg";
+        albumImg.style.boxShadow = "0 4px 8px 0 rgba(255, 255, 255, 0.2), 0 6px 20px 0 rgba(255, 255, 255, 0.19)";
+        links.style.display = "initial";
+        isPress = true;
+    }
+    let num = Math.floor(Math.random()*19);
+    songName.innerHTML = '~   ' + jsongs.songs[num].name + '   ~';
+    spoty.href = jsongs.songs[num].spoty;
+    yt.href = jsongs.songs[num].ytmus;
+    apple.href = jsongs.songs[num].apple;
+    deezer.href = jsongs.songs[num].deezr;
+    cover.href = jsongs.songs[num].genius;
+}
